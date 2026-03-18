028c23208bc6973d005cb6d90a56d7ba5fd50d28
Zacklatta <zalatta@ksu.edu>
2026-03-17 21:34:58 -0500

    Completed the request method

--- src/block_store.c
+++ src/block_store.c
@@ -64,18 +64,56 @@ void block_store_destroy(block_store_t *const bs)
 	}
 }
 
+/**
+ * Searches for first available free block in storage
+ * If free block is found, it marks the block as in use within bitmap and returns block's ID
+ * Returns SIZE_MAX if block store is full, no free blocks are found, or if invalid pointer is found
+ */
 size_t block_store_allocate(block_store_t *const bs)
 {
-	UNUSED(bs);
-	return 0;
+        // Verify that the input pointer is not NULL
+        if(bs == NULL) return SIZE_MAX;
+
+        // Find index of the first free block in bitmap
+        size_t free_block_id = bitmap_ffz(bs->bitmap);
+
+        // Verify it found free block
+        if (free_block_id == SIZE_MAX || free_block_id >= BLOCK_STORE_NUM_BLOCKS) return SIZE_MAX;
+
+        // Request and mark free block as allocated
+        bool success = block_store_request(bs, free_block_id);
+
+        // If unsuccessful, return SIZE_MAX
+        if(!success) return SIZE_MAX;
+
+        // Return ID of allocated block
+        return free_block_id;
 }
 
+
+/**
+ * Checks to see if a given bit is available
+ * if not, then returns false, if so, flips the bit and returns true
+ */
 bool block_store_request(block_store_t *const bs, const size_t block_id)
 {
-	UNUSED(bs);
-	UNUSED(block_id);
-	return false;
+        //checks to see if all parameters are valid
+        if(bs == NULL || bs->bitmap == NULL || block_id >= BLOCK_STORE_NUM_BLOCKS){
+                return false;
+        }
+
+        //gets the bit that represents the block
+        size_t availability = bitmap_test(bs->bitmap, block_id);
+
+        //checks to see if block is already taken (0 if no, 1 if yes)
+        if(availability == 0){
+                bitmap_flip(bs->bitmap, block_id);
+                return true;
+        }
+
+        return false;
 }
+
 
 void block_store_release(block_store_t *const bs, const size_t block_id)
 {
